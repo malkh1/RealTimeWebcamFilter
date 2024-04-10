97b87b10f6fdaa26f7c0d79e7055b5b645702fef
malkh <mohammadalkhaledi2@gmail.com>
2024-04-09 21:08:49 -0400

    Add files via upload

---
+++ src/main/java/ca/comp4102/realtimewebcamfilter/RealTimeWebcamFilter.java
@@ -0,0 +1,12 @@
+package ca.comp4102.realtimewebcamfilter;
+
+/**
+ *
+ * @author mohammad and james
+ */
+public class RealTimeWebcamFilter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
